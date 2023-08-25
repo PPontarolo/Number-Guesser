dbe73a59c63cf8239786d3e66dc403367e684b81
PPontarolo <82400288+PPontarolo@users.noreply.github.com>
2023-08-24 19:16:12 -0700

    Made Craig4.py working

--- Craig4.py
+++ Craig4.py
@@ -3,52 +3,51 @@
 
 ## o(log n)
 
-import random
-import time
-start_time = time.time()
+import random ## for the random number to guess
+import time ## for testing the program time
+start_time = time.time() ## Start
 
-def BinaryS(nums,low,high,ran):
+def BinaryS(arr,low,high,ran):
     
     global count
     count+=1
 
-    if high >= low:
+    if high >= low: ## always goes in here
         middle = (high + low)//2
 
-        if middle == ran:
-            print("Craig guessed {}. Craig guess is too high" .format(middle))
+        if middle == ran: ## guessed the number
+            print("Craig guessed your number!! Your number is {}" .format(middle))
             print("Took Craig {} guesses" .format(count))
 
+            ## for overall average
             global RealCount
             RealCount = RealCount + count
+
             print("-_-_-_-_-_-_-_-_-_-")
             return middle
-        elif middle > ran:
-            print("Middle is ", str(middle))
-            return BinaryS(nums, low, middle-1,ran)
-        else:
-            
+        elif middle > ran: ## too high
+            print("Craig guessed {}. Craig guess is too high" .format(middle))
+            return BinaryS(arr, low, middle-1,ran) ## change high
+        else: ## too low
             print("Craig guessed {}. Craig guess is too low" .format(middle))
-            return BinaryS(nums, middle+1,high,ran)
+            return BinaryS(arr, middle+1,high,ran) ## change low
+    ## dont need an else because it will always be within the bounds of the range
     
 
 if __name__ == '__main__':
     
     start_time = time.time()
-    p = 0
-    RealCount = 0
-    nums = [*range(1,1001,1)]
-    count = 0
-    print(len(nums))
-
+    p = 0;RealCount = 0;count = 0;arr = [*range(1,1001,1)]
+    
     while p < 100:
         ran = random.randint(1, 1000)
         print("Your number is ", str(ran))
-        BinaryS(nums, 0, len(nums), ran)
+        BinaryS(arr, 0, len(arr), ran)
         count = 0
         p+=1
-    print(RealCount/p)
-    print("--- %s seconds ---" % (time.time() - start_time))
+
+    print(RealCount/p) ## gives the average of guesses
+    print("--- %s seconds ---" % (time.time() - start_time)) ## Stop
 
 
 
